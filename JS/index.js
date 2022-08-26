8617f1397f821b3cd777a5c39b660912a0089ffd
Sudhanshu-07 <90274219+Sudhanshu-07@users.noreply.github.com>
2022-08-26 23:51:21 +0530

    Add files via upload

---
+++ JS/index.js
@@ -0,0 +1,25 @@
+
+
+function redirectingni()
+{
+    window.location="north_indian.html";
+}
+function redirectingsi()
+{
+    window.location="South_indian.html";
+}
+function redirectingff()
+{
+    window.location="Fast_food.html";
+}
+function redirectingde()
+{
+    window.location="Desserts.html";
+}
+function redirectingbe()
+{
+    window.location="Beverages.html";
+}
+
+
+
---
+++ JS/order.js
@@ -0,0 +1,161 @@
+// var addItemId=0;
+// function addToCart(MBTN)
+// {
+//     addItemId=addItemId + 1;
+//     var selectedItem=document.createElement('div');
+//     selectedItem.classList.add('cartimg');
+//     selectedItem.setAttribute('id',addItemId);
+//     var img=document.createElement('img');
+//     img.setAttribute('src',MBTN.children[0].currentSrc)
+//     var cartItems =document.getElementById('title');
+//     selectedItem.append(img);
+//     cartItems.append(selectedItem);
+// }
+
+
+
+
+// function addedToCart(){
+//     document.getElementById('addedToCart').innerText;
+// }
+
+document.getElementById('clickedChola').addEventListener("click",()=>{
+    let hellp = document.getElementById('cart');
+    let statement = document.getElementById('title1').innerText + "        ("+  document.getElementById('ruppes').innerText + ")";
+    let li = document.createElement('li');
+    li.classList.add('list-group-item');
+    li.textContent = statement;
+    hellp.appendChild(li);
+});
+document.getElementById('clickedPanner').addEventListener("click",()=>{
+    let hellp = document.getElementById('cart');
+    let statement = document.getElementById('title2').innerText +"    "+  document.getElementById('ruppes').innerText;
+    let li = document.createElement('li');
+    li.classList.add('list-group-item');
+    li.textContent = statement;
+    hellp.appendChild(li);
+});
+document.getElementById('clickedDal').addEventListener("click",()=>{
+    let hellp = document.getElementById('cart');
+    let statement = document.getElementById('title3').innerText+"    "+  document.getElementById('ruppes').innerText;
+    let li = document.createElement('li');
+    li.classList.add('list-group-item');
+    li.textContent = statement;
+    hellp.appendChild(li);
+});
+document.getElementById('clickedPalak').addEventListener("click",()=>{
+    let hellp = document.getElementById('cart');
+    let statement = document.getElementById('title4').innerText+"    "+  document.getElementById('ruppes').innerText;
+    let li = document.createElement('li');
+    li.classList.add('list-group-item');
+    li.textContent = statement;
+    hellp.appendChild(li);
+});
+document.getElementById('clickedKadai').addEventListener("click",()=>{
+    let hellp = document.getElementById('cart');
+    let statement = document.getElementById('title5').innerText+"    "+  document.getElementById('ruppes').innerText;
+    let li = document.createElement('li');
+    li.classList.add('list-group-item');
+    li.textContent = statement;
+    hellp.appendChild(li);
+});
+document.getElementById('clickedSahi').addEventListener("click",()=>{
+    let hellp = document.getElementById('cart');
+    let statement = document.getElementById('title6').innerText+"                  "+  document.getElementById('ruppes').innerText;
+    let li = document.createElement('li');
+    li.classList.add('list-group-item');
+    li.textContent = statement;
+    hellp.appendChild(li);
+});
+document.getElementById('clickedLababdaar').addEventListener("click",()=>{
+    let hellp = document.getElementById('cart');
+    let statement = document.getElementById('title7').innerText+"    "+  document.getElementById('ruppes').innerText;
+    let li = document.createElement('li');
+    li.classList.add('list-group-item');
+    li.textContent = statement;
+    hellp.appendChild(li);
+});
+document.getElementById('clickedMatar').addEventListener("click",()=>{
+    let hellp = document.getElementById('cart');
+    let statement = document.getElementById('title8').innerText+"    "+  document.getElementById('ruppes').innerText;
+    let li = document.createElement('li');
+    li.classList.add('list-group-item');
+    li.textContent = statement;
+    hellp.appendChild(li);
+});
+document.getElementById('clickedTandoori').addEventListener("click",()=>{
+    let hellp = document.getElementById('cart');
+    let statement = document.getElementById('title9').innerText+"    "+  document.getElementById('ruppes').innerText;
+    let li = document.createElement('li');
+    li.classList.add('list-group-item');
+    li.textContent = statement;
+    hellp.appendChild(li);
+});
+// document.getElementById('clickedMatar').addEventListener("click",clicking);
+
+// function clicking1(){
+    
+// }
+// function clicking2(){
+//     let hellp = document.getElementById('cart');
+//     let statement = document.getElementById('title').innerText;
+//     let li = document.createElement('li');
+//     li.textContent = statement;
+//     hellp.appendChild(li);
+// }
+// function clicking3(){
+//     let hellp = document.getElementById('cart');
+//     let statement = document.getElementById('title').innerText;
+//     let li = document.createElement('li');
+//     li.textContent = statement;
+//     hellp.appendChild(li);
+// }
+// function clicking4(){
+//     let hellp = document.getElementById('cart');
+//     let statement = document.getElementById('title').innerText;
+//     let li = document.createElement('li');
+//     li.textContent = statement;
+//     hellp.appendChild(li);
+// }
+// function clicking5(){
+//     let hellp = document.getElementById('cart');
+//     let statement = document.getElementById('title').innerText;
+//     let li = document.createElement('li');
+//     li.textContent = statement;
+//     hellp.appendChild(li);
+// }
+// function clicking6(){
+//     let hellp = document.getElementById('cart');
+//     let statement = document.getElementById('title').innerText;
+//     let li = document.createElement('li');
+//     li.textContent = statement;
+//     hellp.appendChild(li);
+// }
+// function clicking7(){
+//     let hellp = document.getElementById('cart');
+//     let statement = document.getElementById('title').innerText;
+//     let li = document.createElement('li');
+//     li.textContent = statement;
+//     hellp.appendChild(li);
+// }
+// function clicking8(){
+//     let hellp = document.getElementById('cart');
+//     let statement = document.getElementById('title').innerText;
+//     let li = document.createElement('li');
+//     li.textContent = statement;
+//     hellp.appendChild(li);
+// }
+// function clicking9(){
+//     let hellp = document.getElementById('cart');
+//     let statement = document.getElementById('title').innerText;
+//     let li = document.createElement('li');
+//     li.textContent = statement;
+//     hellp.appendChild(li);
+// }
+// function clicking(){
+//     let hellp = document.getElementById('cart');
+//     let statement = document.getElementById('title').innerText;
+//     let li = document.createElement('li');
+//     li.textContent = statement;
+//     hellp.appendChild(li);
+// }
